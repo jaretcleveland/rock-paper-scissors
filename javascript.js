65a0cd6dc1fcbfd9c2601aa6f865ea9c78bd654f
Jaret Cleveland <81397585+jaretcleveland@users.noreply.github.com>
2025-08-07 22:22:12 -0500

    Implement functions & logic to play a single round
    User's choice is compared to the computers and given a score.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,62 @@
-console.log("Hello World")
+
+// Initialize variables to keep track of the players score
+humanScore = 0;
+computerScore = 0;
+
+
+
+// getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”
+
+function getComputerChoice() {
+    const randomNumber = Math.random();
+    if (randomNumber <= 1/3) {
+        return "Rock";
+    }
+    else if (randomNumber <= 2/3) {
+        return "Paper";
+    }
+    else {
+        return "Scissors";
+    }
+}
+
+
+
+// getHumanChoice will return one of the valid choices depending on what the user inputs
+
+function getHumanChoice() {
+    let sign = prompt("Rock, Paper, or Scissors?");
+    return sign;
+}
+
+
+
+
+// playRound takes the human and computer player choices as arguments, plays a single round,
+//  increments the round winner’s score and logs a winner announcement.
+
+function playRound(humanChoice, computerChoice) {
+    const human = humanChoice.toLowerCase(); // set both variables to lower case no matter the input
+    const computer = computerChoice.toLowerCase();
+
+    if (human === computer) {
+        console.log(`It's a tie! You both chose ${human}`);
+    } 
+    else if ((human === "rock" && computer === "scissors") ||
+             (human === "paper" && computer === "rock") ||
+             (human === "scissors" && computer === "paper") ) {
+        humanScore++;
+        console.log(`You win! ${human.charAt(0).toUpperCase() + human.slice(1)} beats ${computer}`);
+    }
+    else {
+        computerScore++;
+        console.log(`You lose! ${computer.charAt(0).toUpperCase() + computer.slice(1)} beats ${human}`);
+    }
+    console.log(`Score - You: ${humanScore}, Computer: ${computerScore}`);
+}
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
